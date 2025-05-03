c0c76684251e154930c8f332c5c0750f621d3b62
bohdandemchukk bc2024-1 <bogdanchuk2006@gmail.com>
2025-05-03 21:29:05 +0300

    Add search bar and improve date and time

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,6 +1,6 @@
 import dayjs from "dayjs";
 import "dayjs/locale/uk";
-
+import {useEffect, useState} from "react"
 dayjs.locale("uk");
 
 
@@ -10,15 +10,40 @@ export default function MovieCard({ movie }) {
     movie.title = "Викрадач коштовностей"
   }
 
-  const formattedDay = dayjs(movie.release_date).format("dddd");
-  const formattedDate = dayjs(movie.release_date).format("D MMMM");
+  const [formattedDay, setFormattedDay] = useState("");
+  const [formattedDate, setFormattedDate] = useState("");
+  const [randomTime, setRandomTime] = useState("");
 
+  useEffect(() => {
 
+    const storedDay = localStorage.getItem(`day-${movie.id}`);
+    const storedDate = localStorage.getItem(`date-${movie.id}`);
+    const storedTime = localStorage.getItem(`time-${movie.id}`);
 
+    if (storedDay && storedDate && storedTime) {
 
-  const randomHour = Math.floor(Math.random() * (22 - 10 + 1)) + 10;
-  const randomMinute = [0, 15, 30, 45][Math.floor(Math.random() * 4)];
-  const randomTime = `${randomHour}:${randomMinute < 10 ? "0" + randomMinute : randomMinute}`;
+      setFormattedDay(storedDay);
+      setFormattedDate(storedDate);
+      setRandomTime(storedTime);
+    } else {
+
+      const newDay = dayjs(movie.release_date).format("dddd");
+      const newDate = dayjs(movie.release_date).format("D MMMM");
+
+      const newHour = Math.floor(Math.random() * (22 - 10 + 1)) + 10;
+      const newMinute = [0, 15, 30, 45][Math.floor(Math.random() * 4)];
+      const newTime = `${newHour}:${newMinute < 10 ? "0" + newMinute : newMinute}`;
+
+      localStorage.setItem(`day-${movie.id}`, newDay);
+      localStorage.setItem(`date-${movie.id}`, newDate);
+      localStorage.setItem(`time-${movie.id}`, newTime);
+
+      setFormattedDay(newDay);
+      setFormattedDate(newDate);
+      setRandomTime(newTime);
+    }
+  }, [movie.id, movie.release_date]);
+
 
   return (
     <div className="relative bg-zinc-900 w-50 h-125 m-5 rounded-lg overflow-hidden rounded-tl-none text-white
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -2,9 +2,10 @@ import MovieCard from "./MovieCard.jsx";
 import {useLoaderData} from "react-router-dom";
 import axios from "axios";
 
+import {useState} from "react"
+
 
 export async function movieLoader() {
-
   const apiKey = import.meta.env.VITE_MOVIES_API_KEY;
 
   const url = `https://api.themoviedb.org/3/movie/now_playing?api_key=${apiKey}&language=uk-UA&page=1`;
@@ -15,14 +16,35 @@ export async function movieLoader() {
 
 export default function MovieList() {
 
+  const [searchValue, setSearchValue] = useState(null)
   const movies = useLoaderData()
+
+  const filteredMovies = searchValue
+    ? movies.filter(movie => {
+      const movieTitle = movie.title.toLowerCase();
+      return [...searchValue.toLowerCase()].every(char => movieTitle.includes(char));
+    })
+    : movies;
+
 
 
   return (
-    <div className="grid grid-cols-6">
-      {movies.map(movie => (
-        <MovieCard key={movie.id} movie={movie} />
-      ))}
-    </div>
+    <div className= "flex flex-col items-center">
+      <input
+        type="text"
+        placeholder="Пошук фільмів"
+        className="w-95/100 p-3 pl-5 mt-3 bg-zinc-900 border border-zinc-700 rounded-3xl text-white
+    placeholder-gray-400 focus:outline-none focus:border-zinc-400 transition-all duration-300
+    hover:border-zinc-500 hover:shadow-lg"
+
+        onChange={(e) => setSearchValue(e.target.value)}
+      />
+
+      <div className="grid grid-cols-6">
+          {filteredMovies.map(movie => (
+            <MovieCard key={movie.id} movie={movie} />
+          ))}
+      </div>
+          </div>
   );
 }
